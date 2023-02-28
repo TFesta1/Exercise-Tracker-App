b226aff9153a06a06ea9e82ab2251b11d06c1e8d
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-02-28 10:44:36 -0500

    Splitting HTML Into Components

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,26 @@
   // Keyword "setup" is required for the script to be recognized as a composition API
   import { RouterLink, RouterView } from 'vue-router'
   import ExerciseView from './views/ExerciseView.vue';
+  import Login from './views/Login.vue';
+  import Register from './views/Register.vue';
   import NavBar from './components/NavBar.vue';
+  import FriendsTable from './components/FriendsTable.vue';
+  import currentWorkout from './components/currentWorkout.vue';
+  import 
+
+  
 
   
 </script>
 
 <template>
-  <NavBar />
+  <ExerciseView />
+  <currentWorkout />
+  <FriendsTable />
+
 
   <div class="container">
-    <RouterView />
+    <!-- <RouterView /> -->
   </div>
 
   <!-- Displays depending on what the RouterLink is -->
