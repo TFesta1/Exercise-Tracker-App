698465978daa85f294c65fdc5cf9b7d913a1bcda
TFesta1 <festat1@hawkmail.newpaltz.edu>
2023-03-01 09:59:41 -0500

    Dynamic Text, RouterLink Implementation W/ CSS

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+    import { ref } from 'vue'
+    import { useRoute } from 'vue-router'
+    import { computed } from 'vue'
     
+
+    const dynamicLabel = computed(() => {
+        const route = useRoute()
+        if (route.path === '/') {
+            return 'Dashboard'
+        } else if (route.path === '/statistics') {
+            return 'Statistics'
+        } else if (route.path === '/activity') {
+            return 'Friends Activity'
+        } else if (route.path === '/search') {
+            return 'People Search'
+        } else if (route.path === '/admin') {
+            return 'Admin'
+        }
+    })
+
 </script>
 
 <template>
@@ -7,22 +27,26 @@
         <div class="logo">
             <!-- Adding the logo -->
             <img src="images/logo.png" alt="logo">
-            <h2 class="dynamic">Dashboard</h2>
+            <h2 class="dynamic">{{ dynamicLabel }}</h2>
 
         </div>
 
         <div class="items">
             <ul>
-                <li><i class="fas fa-pie-chart" aria-hidden="true"><a href="#">Dashboard</a></i></li>
-                <li><i class="fas fa-user" aria-hidden="true"><a href="#">Statistics</a></i></li>
-                <li><i class="fas fa-id-badge" aria-hidden="true"><a href="#">Friends Activity</a></i></li>
-                <li><i class="fas fa-filter" aria-hidden="true"><a href="#">People Search</a></i></li>
-                <li><i class="fas fa-lock" aria-hidden="true"><a href="#">Admin</a></i></li>
+                <!-- Add the "add-border" class when we're at the respective RouterLinks. Also, RouterLinks redirect the page when clicked -->
+                <RouterLink to='/'><li :class="{ 'add-border': $route.path === '/' }"><i class="fas fa-pie-chart" aria-hidden="true"><a href="#">Dashboard</a></i></li></RouterLink>
+                <RouterLink to='/statistics'><li :class="{ 'add-border': $route.path === '/statistics' }"><i class="fas fa-user" aria-hidden="true"><a href="#">Statistics</a></i></li></RouterLink>
+                <RouterLink to='/activity'><li :class="{ 'add-border': $route.path === '/activity' }"><i class="fas fa-id-badge" aria-hidden="true"><a href="#">Friends Activity</a></i></li></RouterLink>
+                <RouterLink to='/search'><li :class="{ 'add-border': $route.path === '/search' }"><i class="fas fa-filter" aria-hidden="true"><a href="#">People Search</a></i></li></RouterLink>
+                <RouterLink to='/admin'><li :class="{ 'add-border': $route.path === '/admin' }"><i class="fas fa-lock" aria-hidden="true"><a href="#">Admin</a></i></li></RouterLink>
             </ul>
         </div>
     </section>
 </template>
 
 <style scoped>
+    /* #menu-item1 {
+        border-left: none;
+    } */
 
 </style>
---
+++ client/src/components/PfpSearch.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="exerciseStyle.css">
+        
+
+        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/> -->
+        <!-- jQuery -->
+        <!-- <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script> -->
+        <title>Exercise Tracker</title>
+    </head>
+    <body>
+        
+        
+
+        <section id="interface">
+            <div class="navigation">
+                <div class="n1" style="">
+                    <!-- <div>
+                        <i id="menu-btn" class="fa fa-bars"></i>
+                    </div> -->
+                    <div class="search">
+                        <!-- Icon -->
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        <!-- Input field -->
+                        <input type="text" placeholder="Search">
+                    </div>
+                </div>
+
+                <!-- Bell icon and pfp on top right -->
+                <div class="profile">
+                    <i class="fa fa-bell" aria-hidden="true"></i>
+                    <img src="images/1.png" alt="pfp">
+                </div>
+            </div>
+
+            <!-- The title -->
+            <h3 class="i-name">
+                <!-- Dashboard -->
+                <button class="add-workout">
+                    <i></i>
+                    Add Workout
+                </button>
+            </h3>
+
+            
+
+            
+
+            
+        </section>
+        
+        
+    </body>
+    </html>
+</template>
+
+<style scoped>
+
+</style>
