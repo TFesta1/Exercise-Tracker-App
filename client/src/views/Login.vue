a5310aecd1c69bd8508cc6447e3e2dc7ac24780d
Tanner Festa <festat1@newpaltz.edu>
2023-05-02 12:33:59 -0400

    Google Login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,17 +4,36 @@
     // import userStore from "@/stores/user";
     import { onMounted, provide, inject, ref  } from "vue";
     import type UserStore from '@/stores/user';
+    import { loadScript, rest } from '@/model/myFetch';
     const userStore = inject('userStore') as typeof UserStore;
-    // @ goes to the src file
-    // import router from '@/router/index'
-
-
-    // import users from "@/../public/data/users.json"
-
-    // import { UserStore } from '@/stores/user';
-
-    // onMounted(userStore.getUser)
-    // provide("userStore", userStore)
+    
+    async function googleLogin()
+    {
+        await loadScript('https://accounts.google.com/gsi/client', 'google-login');
+        //await loadScript('https://apis.google.com/js/platform.js', 'gapi');
+        const client = google.accounts.oauth2.initTokenClient({
+            client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+            scope: 'https://www.googleapis.com/auth/calendar.readonly \
+                    https://www.googleapis.com/auth/contacts.readonly',
+            callback: async (tokenResponse: string) => {
+                console.log(tokenResponse);
+                const me = await rest(
+                    'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses',
+                    null, undefined, {
+                        "Authorization": "Bearer " + tokenResponse.access_token
+                    }
+                    
+                    );
+                console.log(me);
+                const calendar = await rest('https://www.googleapis.com/calendar/v3/calendars/primary/events',
+                    null, undefined, {
+                        "Authorization": "Bearer " + tokenResponse.access_token
+                    })
+                console.log(calendar);
+            },
+            });
+            client.requestAccessToken();
+    }
 
     // Bad code since it's using "as any" but it's because it's not a defined type. It's just explicitly exported variables
     const router = inject('router') as any;
@@ -38,6 +57,7 @@
     
 
 </script>
+
 
 <template>
     <!DOCTYPE html>
@@ -89,6 +109,11 @@
         </div>
 
     </form>
+    <p class="googleButtonWrapper">
+        <button class="button is-primary googleButton" @click="googleLogin">
+            Login With Google
+        </button>
+    </p>
 
     <div class="display-pass-user-list">
         <h2>FOR TESTING ONLY</h2>
@@ -99,5 +124,37 @@
 
 <style scoped>
     @import "@/assets/auth.css";
+    .googleButtonWrapper {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+    .googleButton {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        padding: 12px 24px;
+        border: none;
+        border-radius: 4px;
+        font-size: 16px;
+        font-weight: bold;
+        color: #fff;
+        background-color: #67941a;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    .googleButton:hover {
+        background-color: #7f9c3a;
+    }
+
+    .googleButton:active {
+        background-color: #9cc12d;
+    }
+
+    .googleButton:focus {
+        outline: none;
+        box-shadow: 0px 0px 0px 4px rgba(66, 133, 244, 0.4);
+    }
 
 </style>
